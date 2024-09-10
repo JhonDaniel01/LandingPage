dfc439faa4546e4a2f378eea728860b558185134
JhonDaniel01 <jhon-coso@hotmail.com>
2024-09-10 12:07:33 -0500

    Image Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,51 @@
 import React from "react";
+import { RiCircleFill, RiPlayFill } from "react-icons/ri";
 
 const Hero = () => {
   return (
     <section className="h-[90vh] grid grid-cols-1 md:grid-cols-8">
-      {/*  */}
-      <div className="bg-red-400 md:col-span-5 flex items-center justify-center p-8">
-        <div>
-          <h1 className="text-7xl font-bold">
+      {/* Information */}
+      <div className=" md:col-span-5 flex items-center justify-center p-16">
+        <div className="flex flex-col gap-8">
+          <h1 className="text-7xl font-bold leading-[7rem]">
             Web Design Impactful Digital{" "}
-            <span className="text-primary p-2 border-8 border-primary">
+            <span className="text-primary py-2 px-6 border-8 border-primary relative">
               Products
+              <RiCircleFill className="text-base  absolute -left-5 -top-5 text-white p-2 bg-primary rounded-full box-content" />
+              <RiCircleFill className="text-base  absolute -right-5 -top-5 text-white p-2 bg-primary rounded-full box-content" />
+              <RiCircleFill className="text-base  absolute -left-5 -bottom-5 text-white p-2 bg-primary rounded-full box-content" />
+              <RiCircleFill className="text-base  absolute -right-5 -bottom-5 text-white p-2 bg-primary rounded-full box-content" />
             </span>
           </h1>
-          <p>Description</p>
-          <p>Butons</p>
+          <p className="text-gray-500 text-2xl leading-[2.5rem]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta
+            diam libero, in ultricies sapien sodales id. Vestibulum ante ipsum
+            primis in faucibus orci luctus et ultrices posuere cubilia curae;
+            Sed sed nisi augue.
+          </p>
+          <div className="flex items-center gap-8">
+            <button className="bg-primary text-white py-2 px-6 rounded-xl text-xl">
+              Contact Us
+            </button>
+            <button className="flex items-center gap-4 py-2 px-6 rounded-xl text-xl text-left">
+              <RiPlayFill className=" bg-secondary text-primary p-4 box-content rounded-full" />{" "}
+              Watch our <br />
+              introduction video
+            </button>
+          </div>
         </div>
       </div>
-      <div className="bg-red-600  md:col-span-3">Image</div>
+      {/* Imagen */}
+      <div className="md:col-span-3 flex items-center justify-center relative">
+        <div>
+          <img
+            src="hero.png"
+            className="w-[450px] h-[450px] object-cover -mt-28"
+          />
+          <div></div>
+        </div>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[380px] w-[380px] bg-secondary border-[10px] border-primary rounded-full -z-10 "></div>
+      </div>
     </section>
   );
 };
